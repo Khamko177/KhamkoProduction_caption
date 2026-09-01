860fddf8352685cd7a1afc58ae29f8e0ca8cf193
Khamko <khamko@pascaliaasia.com>
2026-09-01 11:07:36 +0700

    ci: add GitHub Pages auto deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-﻿import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Universal relative base for GitHub Pages, Vercel, Netlify
 });
